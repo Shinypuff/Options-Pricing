878192ffd4156b1be2e22b638add0fff006fe091
PlatinumBeast88 <105783983+PlatinumBeast88@users.noreply.github.com>
2023-09-19 20:06:10 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from dash import dcc
 from dash import html
 from datetime import date
 from back import Option
+from back import get_board
 import numpy as np
 import time
 import pandas as pd
@@ -17,7 +18,7 @@ empty_tree = pd.DataFrame(index=range(10), columns= [" "] * 11)
 app = dash.Dash(__name__)
 
 app.layout = html.Div(id="page", children=(html.Div(id="input_cells", children=(html.H1(id='text', children = "Ввод"),
-                                html.Div(id='dropdowns', children=(dcc.Dropdown(id='asset_list', options=["ABMN", "AMZN"], placeholder='Наименование актива', style = {"marginTop" : "20px", "width" : "317px"}))),
+                                html.Div(id='dropdowns', children=(dcc.Dropdown(id='asset_list', options=["YNDX", "SBER", "RUAL"], placeholder='Наименование актива', style = {"marginTop" : "20px", "width" : "317px"}))),
                                 html.Div([dcc.RadioItems(id='type_choice', options=['Европейский', 'Американский'], inline=True)], style = {"margin" : "10px 0px 0px 40px"}),
                                 html.Div([dcc.Input(id='price_field', value=100, type='number', placeholder='Цена актива', style= {"width": "308px","marginTop" : "10px"})]),
                                 html.Div([dcc.Input(id='strike_field', value=100, type='number', placeholder='Цена исполнения', style= {"width": "308px","marginTop" : "10px"})]),
@@ -29,27 +30,27 @@ app.layout = html.Div(id="page", children=(html.Div(id="input_cells", children=(
                       html.Div(id="calculator_div", children=(html.Div(id='results', children=(html.Div(id='put_part', children=(html.H1(id='calculated', children='Результаты'), html.Div(id='table_update', children=dash_table.DataTable(blank.to_dict('records'), [{"name": i, "id": i} for i in blank.columns], id='greeks', style_header={'backgroundColor': 'red','fontWeight': 'bold'}, style_as_list_view=True))), style={'display':'inline-block'})), style={'display' : 'flex'})),
                                style={'display':'inline-block', 'width':'19%'}),
                       html.Div(id="tree_call", children=(html.Div(id="pretty_tree", children=(dash_table.DataTable())), html.Div(id="pretty_opt", children=(dash_table.DataTable()))),style={"display" : "inline-block"}),
-                      html.Div(id="tree_put", children=(html.Div(id="pretty_tree_put", children=(dash_table.DataTable())), html.Div(id="pretty_opt_put", children=(dash_table.DataTable()))),style={"display" : "inline-block"})), style={'display':'flex'})
+                      html.Div(id="tree_put", children=(html.Div(id="pretty_tree_put", children=(dash_table.DataTable())), html.Div(id="pretty_opt_put", children=(dash_table.DataTable()))),style={"display" : "inline-block"}),
+                      html.Div(children=[dbc.Row(id='graph', children=dcc.Graph()), dbc.Row(id='tables', children=[dash_table.DataTable(), dash_table.DataTable()])],id="graph_table", style = {'display' : 'inline-block'})), style={'display':'flex'})
 
 
-@app.callback(Output("dropdowns", "children"), Input("asset_list", "value"))
+@app.callback([[Output("price_field", "value"), Output("strike_field", "value"), Output("sigma_field", "value")],
+               Output("graph", "children"), Output("tables", "children")],
+              Input("asset_list", "value"))
 def get_attributes(asset):
 
-    if asset!=None:
-        return (dcc.Dropdown(id='asset_list', options=["ABMN", "AMZN"], value=asset, placeholder='Наименование актива', style={"marginTop": "20px", "width": "317px"}),
-                dcc.Dropdown(id='stock_future_list',
-                             options=["Фьючерсы", "Акции", None],
-                             value='string',
-                             placeholder='Тип базового актива',
-                             style={"marginTop": "20px", "width": "317px"}))
-    return (dcc.Dropdown(id='asset_list',
-                         options=["ABMN", "AMZN"],
-                         value=asset,
-                         placeholder='Наименование актива',
-                         style={"marginTop": "20px", "width": "317px"}))
+    call, put, params, fig = get_board(asset)
+    print(call)
+    return params, dcc.Graph(figure=fig), [dash_table.DataTable(call.to_dict('records'), [{"name": i, "id": i} for i in call.columns]),
+                                           dash_table.DataTable(put.to_dict('records'), [{"name": i, "id": i} for i in put.columns])]
 
-
-@app.callback([Output("table_update", "children"), Output("pretty_tree", "children"), Output("pretty_opt", "children"), Output("pretty_tree_put", "children"), Output("pretty_opt_put", "children")], [Input("price_field", "value"), Input("strike_field", "value"), Input("sigma_field", "value"), Input("risk_free_field", "value"), Input("start_date_field", 'start_date'), Input("start_date_field", 'end_date'), Input("type_choice", "value")])
+@app.callback([Output("table_update", "children"), Output("pretty_tree", "children"),
+               Output("pretty_opt", "children"), Output("pretty_tree_put", "children"),
+               Output("pretty_opt_put", "children")],
+              [Input("price_field", "value"), Input("strike_field", "value"),
+               Input("sigma_field", "value"), Input("risk_free_field", "value"),
+               Input("start_date_field", 'start_date'), Input("start_date_field", 'end_date'),
+               Input("type_choice", "value")])
 def table(price, strike, sigma,risk_free, start_date, end_date, type_choice):
 
     check_val = price, strike, sigma, risk_free, start_date, end_date
